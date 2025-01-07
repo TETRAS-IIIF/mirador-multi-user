e0647a4a89f7273cd79ebde3c353b5f16a1ebdc6
Drilio <Roy-Antoine93@proton.me>
2025-01-07 09:15:11 +0100

    fix bug where user couldn't added manifest to group

--- backend/src/LinkModules/link-manifest-group/dto/add-manifest-to-group.dto.ts
+++ backend/src/LinkModules/link-manifest-group/dto/add-manifest-to-group.dto.ts
@@ -1,11 +1,12 @@
 import { ManifestGroupRights } from '../../../enum/rights';
-import { IsEnum, IsNumber } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional } from "class-validator";
 
 export class AddManifestToGroupDto {
   @IsNumber()
   userGroupId: number;
   @IsNumber()
   manifestId: number;
+  @IsOptional()
   @IsEnum(ManifestGroupRights)
   rights?: ManifestGroupRights;
 }
