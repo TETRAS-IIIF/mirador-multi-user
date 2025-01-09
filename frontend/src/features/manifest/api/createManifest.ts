07db3fc22d4dce76109955c6c49222b821b89d0d
Drilio <Roy-Antoine93@proton.me>
2025-01-09 11:58:29 +0100

    better handle of error

--- frontend/src/features/manifest/api/createManifest.ts
+++ frontend/src/features/manifest/api/createManifest.ts
@@ -1,5 +1,6 @@
 import storage from "../../../utils/storage.ts";
 import { manifestCreationDto } from "../types/types.ts";
+import toast from "react-hot-toast";
 
 export const createManifest = async (createManifestDto:manifestCreationDto) => {
   const token = storage.getToken();
@@ -13,6 +14,13 @@ export const createManifest = async (createManifestDto:manifestCreationDto) => {
       body: JSON.stringify(createManifestDto)
     });
     const manifest = await response.json();
+    if (!response.ok) {
+      if (response.status === 400) {
+        toast.error(manifest.message);
+        throw new Error('invalid format')
+      }
+      throw new Error(`Error: ${response.statusText}`);
+    }
     return manifest
   } catch (error) {
     throw error;
