bbbbab926a0e8230116e0485ad134724bbd8d1c5
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-01-20 21:54:24 +0100

    Finally preview works from direct file

--- frontend/src/features/manifest/component/MediaVideoThumbnail.tsx
+++ frontend/src/features/manifest/component/MediaVideoThumbnail.tsx
@@ -1,15 +1,18 @@
 import { Box, Grid } from "@mui/material";
-import ReactPlayer from "react-player";
 import React, { useRef } from "react";
 
 export function MediaVideoThumbnail({ media }) {
 
-  const playerRef = useRef(null);
+  const handleLoadedMetadata = () => {
+    const video = videoEl.current;
+    if (!video) return;
+    console.log(`The video is ${video.duration} seconds long.`);
+    console.log(`The video is ${video.videoHeight} height.`);
+    console.log(`The video is ${video.videoWidth} width.`);
 
-  if (playerRef.current) {
-    console.log(playerRef.current.getInternalPlayer().thumbnail);
-    console.log(playerRef.current.getDuration());
-  }
+  };
+
+  const videoEl = useRef(null);
 
   return (
     <Grid>
@@ -19,7 +22,7 @@ export function MediaVideoThumbnail({ media }) {
           src={media.thumbnailUrl}
           loading="lazy"
           sx={{
-            width: 100,
+            width: 200,
             height: 50,
             objectFit: "cover",
             "@media(min-resolution: 2dppx)": {
@@ -31,35 +34,15 @@ export function MediaVideoThumbnail({ media }) {
       }
       {!media.thumbnailUrl && (
         <>
-          <ReactPlayer
-            url={media.value}
-            width={0}
-            height={0}
-            controls={false}
-            light={true}
-            playing={false}
-            muted={true}
-            ref={playerRef}
+          <video
+            width="200"
+            ref={videoEl}
+            src={media.value}
+            controls
+            onLoadedMetadata={handleLoadedMetadata}
+
           />
-          {playerRef.current && (
-            <>
-              <Box
-                component="img"
-                src={playerRef.current.getInternalPlayer().thumbnail}
-                loading="lazy"
-                sx={{
-                  width: 100,
-                  height: 50,
-                  objectFit: "cover",
-                  "@media(min-resolution: 2dppx)": {
-                    width: 100,
-                    height: 100
-                  }
-                }}
-              />
-              <h1>{playerRef.current.getDuration()}</h1>
-            </>
-          )}
+
         </>
       )}
     </Grid>
