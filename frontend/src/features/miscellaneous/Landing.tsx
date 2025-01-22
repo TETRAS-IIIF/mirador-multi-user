6539da953fb7c8873f1cee28bbf409b2bcb74670
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-01-22 22:22:17 +0100

    WIPuse custom asset

--- frontend/src/features/miscellaneous/Landing.tsx
+++ frontend/src/features/miscellaneous/Landing.tsx
@@ -1,50 +1,57 @@
-import { useNavigate } from "react-router-dom";
-import { Button, Grid, Typography } from "@mui/material";
-import { theme } from "../../assets/theme/mainTheme.ts";
-import { useTranslation } from "react-i18next";
+import { useNavigate } from 'react-router-dom';
+import { Button, Grid, Typography } from '@mui/material';
+import { theme } from '../../assets/theme/mainTheme.ts';
+import { useTranslation } from 'react-i18next';
 
 export const Landing = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
-  const HandleSignIn= ()=>{
+  const HandleSignIn = () => {
     navigate('/auth/signin');
   }
-  const HandleLogin= ()=>{
-    navigate( '/auth/login');
+  const HandleLogin = () => {
+    navigate('/auth/login');
   }
-  return(
-    <Grid
-    container
-    direction="column"
-    justifyContent="center"
-    alignItems="center"
-    spacing={10}
-    minHeight={'100vh'}
-    sx={{
-      backgroundImage: theme.palette.backgroundImage,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      marginTop:0
-    }}
-    >
-      <Grid item>
-      <Typography variant="h2" component="h1">{t('welcome', { instanceName : import.meta.env.VITE_INSTANCE_NAME})}</Typography>
-      </Grid>
+  return (
+    <>
       <Grid
-        item
         container
+        direction="column"
         justifyContent="center"
-        spacing={5}
         alignItems="center"
+        spacing={10}
+        minHeight={'100vh'}
+        sx={{
+          backgroundImage: theme.palette.backgroundImage,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          marginTop: 0,
+        }}
       >
         <Grid item>
-          <Button  variant="contained" onClick={HandleSignIn}>{t('create-account')}</Button>
+          <Typography variant="h2"
+                      component="h1">{t('welcome', { instanceName: import.meta.env.VITE_INSTANCE_NAME })}</Typography>
         </Grid>
-        <Grid item>
-          <Button  variant="contained" onClick={HandleLogin}>{t('login')}</Button>
+        <Grid
+          item
+          container
+          justifyContent="center"
+          spacing={5}
+          alignItems="center"
+        >
+          <Grid item>
+            <Button variant="contained" onClick={HandleSignIn}>{t('create-account')}</Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={HandleLogin}>{t('login')}</Button>
+          </Grid>
+        </Grid>
+        <Grid item width={'100%'}>
+          <iframe src="./../../public/customAssets/landing-footer.html" width={'100%'} height={100}
+                  style={{ border: 'none' }}></iframe>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   )
 }
