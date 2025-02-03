139948cd753e979e23e4bfe6953410b0e2daa021
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-02-03 20:05:54 +0100

    Remove useless useeffect

--- frontend/src/features/projects/components/ProjectIframe.tsx
+++ frontend/src/features/projects/components/ProjectIframe.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Box, Button, Grid, TextField } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { MMUModal } from "../../../components/elements/modal.tsx";
@@ -11,17 +11,10 @@ interface IframeGeneratorProps {
 }
 
 export const IframeGenerator = ({ snapshotUrl }: IframeGeneratorProps) => {
-  const [iframeCode, setIframeCode] = useState<string>("");
   const [openIframeModal, setOpenIframeModal] = useState(false);
   const { t } = useTranslation();
 
-  useEffect(() => {
-    if (snapshotUrl) {
-      setIframeCode(
-        `<iframe src="${snapshotUrl}" width="100%" height="600px" style="border:none;" title="Project Snapshot"></iframe>`,
-      );
-    }
-  }, [snapshotUrl]);
+  const iframeCode = `<iframe src="${snapshotUrl}" width="100%" height="600px" style="border:none;" title="Project Snapshot"></iframe>`;
 
   const handleCopyToClipboard = async () => {
     if (iframeCode) {
