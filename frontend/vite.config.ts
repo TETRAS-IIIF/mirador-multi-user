0ce53a40cf71ee1ebafab109f558972dbdf54927
Drilio <Roy-Antoine93@proton.me>
2025-02-26 10:26:23 +0100

    update vite config to handle text editor

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: "window", // 👈 Define `global` for browser usage
+    global: "window",
   },
   plugins: [
     {
