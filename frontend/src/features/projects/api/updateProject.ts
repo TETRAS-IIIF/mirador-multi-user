09754e31c4622bd7a36bb601e29cdae947bff775
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-03-07 10:13:31 +0100

    Improve fetchBackendAPIConnected

--- frontend/src/features/projects/api/updateProject.ts
+++ frontend/src/features/projects/api/updateProject.ts
@@ -1,12 +1,22 @@
 import { ProjectGroupUpdateDto } from "../types/types";
 import { fetchBackendAPIConnected } from "../../../utils/fetchBackendAPI.ts";
+import toast from "react-hot-toast";
 
 export const updateProject = async (project: ProjectGroupUpdateDto) => {
-  return await fetchBackendAPIConnected(`link-group-project/updateProject`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
+  return await fetchBackendAPIConnected(
+    `link-group-project/updateProject`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
     },
-    body: JSON.stringify(project),
-  });
+    () => {
+      toast.success(`Project ${project.project.title} saved`); // TODO Trad
+    },
+    () => {
+      toast.error(`Failed to save ${project.project.title}`); // TODO Trad
+    },
+  );
 };
