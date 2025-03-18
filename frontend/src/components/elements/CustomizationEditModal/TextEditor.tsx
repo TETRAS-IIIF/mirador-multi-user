f97effc145d1e825800312245365c46adafde96b
Drilio <Roy-Antoine93@proton.me>
2025-03-18 11:58:47 +0100

    improved react quill tools

--- frontend/src/components/elements/CustomizationEditModal/TextEditor.tsx
+++ frontend/src/components/elements/CustomizationEditModal/TextEditor.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import "react-quill/dist/quill.bubble.css";
 import { styled } from "@mui/material/styles";
 import { useTranslation } from "react-i18next";
 
@@ -33,11 +34,45 @@ export const TextEditor: React.FC<TextEditorProps> = ({
     updateText(html);
   };
 
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      [{ color: [] }, { background: [] }],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "color",
+    "background",
+  ];
+
   return (
     <StyledReactQuill
       value={editorHtml}
       onChange={handleChange}
       placeholder={t("yourTextHere")}
+      modules={modules}
+      formats={formats}
     />
   );
 };
