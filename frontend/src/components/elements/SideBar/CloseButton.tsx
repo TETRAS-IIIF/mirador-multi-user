d69b324a55cc9929ee0f93edc19f22c7b16fd2c1
Drilio <Roy-Antoine93@proton.me>
2025-03-20 11:39:41 +0100

    passed it with theme

--- frontend/src/components/elements/SideBar/CloseButton.tsx
+++ frontend/src/components/elements/SideBar/CloseButton.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, useTheme } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 interface CloseButtonProps {
@@ -7,26 +7,29 @@ interface CloseButtonProps {
 }
 
 export const CloseButton = ({ text, isSelected }: CloseButtonProps) => {
+  const theme = useTheme();
+
   return (
     <Grid
       container
       justifyContent="center"
       alignItems="center"
       sx={{
-        backgroundColor: isSelected ? "#1976d2" : "#fff",
+        backgroundColor: isSelected
+          ? theme.palette.selectedBackground
+          : theme.palette.background.paper,
         padding: "5px",
         borderRadius: 1,
         transformOrigin: "0 0",
         transform: "rotate(-90deg)",
-        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.8)",
+        boxShadow: theme.shadows[4],
       }}
     >
       <Typography
         variant="body2"
-        color={isSelected ? "#F5EBFF" : "#1976d2"}
-        sx={{
-          textColor: isSelected ? "white" : "inherit",
-        }}
+        color={
+          isSelected ? theme.palette.selectedText : theme.palette.primary.main
+        }
       >
         {text}
       </Typography>
