02694e341b4ee8c05a7e51ac80eab40c1c423a58
Drilio <Roy-Antoine93@proton.me>
2025-04-07 15:54:12 +0200

    Using reactQuerry to request for setting

--- frontend/src/features/auth/routes/Login.tsx
+++ frontend/src/features/auth/routes/Login.tsx
@@ -1,21 +1,29 @@
-import { Layout } from "../components/layout";
-import { LoginForm } from "../components/LoginForm";
-import { Grid, Typography } from "@mui/material";
-import { NavLink } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { Layout } from '../components/layout';
+import { LoginForm } from '../components/LoginForm';
+import { Grid, Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useAdminSettings } from '../../../utils/customHooks/useAdminSettings.ts';
 
 export const Login = () => {
   const { t } = useTranslation();
+  const { data: settings } = useAdminSettings();
+
+  const displayRegisterButton =
+    settings?.mutableSettings.find((s) => s.key === 'DISPLAY_USER_INSCRIPTION_PAGE')?.value === 'true' ||
+    settings?.unMutableSettings.find(([key]) => key === 'DISPLAY_USER_INSCRIPTION_PAGE')?.[1] === 'true';
 
   return (
     <Layout
-      title={t("loginTitle")}
+      title={t('loginTitle')}
       rightButton={
-        <Grid>
-          <NavLink to="/auth/signin">
-            <Typography variant="button">{t("register")}</Typography>
-          </NavLink>
-        </Grid>
+        displayRegisterButton && (
+          <Grid>
+            <NavLink to="/auth/signin">
+              <Typography variant="button">{t('register')}</Typography>
+            </NavLink>
+          </Grid>
+        )
       }
     >
       <Grid item>
--- frontend/src/features/auth/routes/index.tsx
+++ frontend/src/features/auth/routes/index.tsx
@@ -1,12 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import { Login } from "./Login.tsx";
-import { Register } from "./Register.tsx";
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './Login.tsx';
+import { Register } from './Register.tsx';
+import { useAdminSettings } from '../../../utils/customHooks/useAdminSettings.ts';
+import { LoadingSpinner } from '../../../components/elements/loadingSpinner.tsx';
 
 export const AuthRoutes = () => {
+  const { data: settings, isLoading } = useAdminSettings();
+
+  if (isLoading) return <LoadingSpinner />;
+
+  const getSettingValue = (key: string) =>
+    settings?.mutableSettings.find((s) => s.key === key)?.value ??
+    settings?.unMutableSettings.find(([k]) => k === key)?.[1];
+
+  const showInscription = getSettingValue('DISPLAY_USER_INSCRIPTION_PAGE') === 'true';
+
   return (
     <Routes>
       <Route path="/login" Component={Login} />
-      <Route path="/signin" Component={Register} />
+      {showInscription && <Route path="/signin" Component={Register} />}
     </Routes>
   );
 };
