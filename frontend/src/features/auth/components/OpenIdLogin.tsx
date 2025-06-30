57ca5f5f9977ce8ec3d843c34fd909282cc03894
Drilio <Roy-Antoine93@proton.me>
2025-06-30 15:46:31 +0200

    update openid redirect url

--- frontend/src/features/auth/components/OpenIdLogin.tsx
+++ frontend/src/features/auth/components/OpenIdLogin.tsx
@@ -1,16 +1,19 @@
 import { Button, Grid } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { OIDC_CLIENT_ID, OPEN_ID_URL } from '../../../utils/utils.ts';
+import {
+  OIDC_CLIENT_ID,
+  OIDC_REDIRECT_URI,
+  OPEN_ID_URL,
+} from '../../../utils/utils.ts';
 
 export const OpenIdLogin = () => {
   const { t } = useTranslation();
 
   function redirectToOpenIdLogin() {
-    const redirectUri = `${window.location.origin}/auth/openId-callback`;
     window.location.href =
       `${OPEN_ID_URL}/protocol/openid-connect/auth` +
       `?client_id=${OIDC_CLIENT_ID}` +
-      `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+      `&redirect_uri=${encodeURIComponent(OIDC_REDIRECT_URI)}` +
       `&response_type=code` +
       `&scope=openid%20email%20profile`;
   }
