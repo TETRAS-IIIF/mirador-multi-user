e1a3fce150f0e227fd6be519b1e6490858b803bf
Drilio <Roy-Antoine93@proton.me>
2025-07-17 13:41:16 +0200

    improve ui when classic login is disabled

--- frontend/src/features/miscellaneous/Landing.tsx
+++ frontend/src/features/miscellaneous/Landing.tsx
@@ -5,27 +5,42 @@ import { useTranslation } from 'react-i18next';
 import { LandingFooter } from '../../../customAssets/landing-footer.tsx';
 import { useAdminSettings } from '../../utils/customHooks/useAdminSettings.ts';
 import { LoadingSpinner } from '../../components/elements/loadingSpinner.tsx';
-import { SettingKeys } from '../../utils/utils.ts';
+import {
+  getSettingValue,
+  OIDC_CLIENT_ID,
+  OIDC_REDIRECT_URI,
+  OPEN_ID_URL,
+  SettingKeys,
+} from '../../utils/utils.ts';
 
 export const Landing = () => {
   const navigate = useNavigate();
   const { data: settings, isLoading } = useAdminSettings();
+
+  const allowClassic =
+    getSettingValue(SettingKeys.CLASSIC_AUTHENTICATION, settings) === 'true';
+  const allowOpenId =
+    getSettingValue(SettingKeys.OPENID_CONNECTION, settings) === 'true';
 
   const { t } = useTranslation();
   const HandleSignIn = () => {
     navigate('/auth/signin');
   };
   const HandleLogin = () => {
-    navigate('/auth/login');
+    if (!allowClassic && allowOpenId) {
+      window.location.href =
+        `${OPEN_ID_URL}/protocol/openid-connect/auth` +
+        `?client_id=${OIDC_CLIENT_ID}` +
+        `&redirect_uri=${encodeURIComponent(OIDC_REDIRECT_URI)}` +
+        `&response_type=code` +
+        `&scope=openid%20email%20profile`;
+    } else {
+      navigate('/auth/login');
+    }
   };
 
-
-  const getSettingValue = (key: string) =>
-    settings?.mutableSettings.find((s) => s.key === key)?.value ??
-    settings?.unMutableSettings.find(([k]) => k === key)?.[1];
-
-  const showInscription = getSettingValue(SettingKeys.ALLOW_NEW_USER) === 'true';
-
+  const showInscription =
+    getSettingValue(SettingKeys.ALLOW_NEW_USER, settings) === 'true';
   return (
     <Grid
       container
@@ -42,42 +57,40 @@ export const Landing = () => {
         marginTop: 0,
       }}
     >
-      {
-        isLoading ? (
-          <LoadingSpinner />
-        ) : (
-          <>
-            <Grid item>
-              <Typography variant="h2" component="h1">
-                {t('welcome', { instanceName: import.meta.env.VITE_INSTANCE_NAME })}
-              </Typography>
-            </Grid>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <Grid item>
+            <Typography variant="h2" component="h1">
+              {t('welcome', {
+                instanceName: import.meta.env.VITE_INSTANCE_NAME,
+              })}
+            </Typography>
+          </Grid>
 
-            <Grid
-              item
-              container
-              justifyContent="center"
-              spacing={5}
-              alignItems="center"
-            >
-              {
-                showInscription && (
-                  <Grid item>
-                    <Button variant="contained" onClick={HandleSignIn}>
-                      {t('create-account')}
-                    </Button>
-                  </Grid>
-                )
-              }
+          <Grid
+            item
+            container
+            justifyContent="center"
+            spacing={5}
+            alignItems="center"
+          >
+            {showInscription && (
               <Grid item>
-                <Button variant="contained" onClick={HandleLogin}>
-                  {t('login')}
+                <Button variant="contained" onClick={HandleSignIn}>
+                  {t('create-account')}
                 </Button>
               </Grid>
+            )}
+            <Grid item>
+              <Button variant="contained" onClick={HandleLogin}>
+                {t('login')}
+              </Button>
             </Grid>
-          </>
-        )
-      }
+          </Grid>
+        </>
+      )}
       <Grid item width={'100%'}>
         <LandingFooter></LandingFooter>
       </Grid>
