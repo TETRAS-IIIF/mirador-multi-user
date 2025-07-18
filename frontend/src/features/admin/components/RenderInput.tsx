ca9f4bcd3adad0a141987a79c82e7275e07393f6
Drilio <Roy-Antoine93@proton.me>
2025-07-18 09:50:30 +0200

    Tooltip for open id

--- frontend/src/features/admin/components/RenderInput.tsx
+++ frontend/src/features/admin/components/RenderInput.tsx
@@ -1,16 +1,22 @@
 import { Setting } from '../types/type.ts';
-import { FormControlLabel, InputAdornment, Switch, TextField, Tooltip } from '@mui/material';
+import {
+  FormControlLabel,
+  InputAdornment,
+  Switch,
+  TextField,
+  Tooltip,
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { SettingKeys } from '../../../utils/utils.ts';
 
 interface IRenderInputProps {
   setting: Setting;
-  handleChange: (settingId: number, newValue: string) => void
+  handleChange: (settingId: number, newValue: string) => void;
 }
 
 export const RenderInput = ({ setting, handleChange }: IRenderInputProps) => {
   const isBoolean = setting.value === 'true' || setting.value === 'false';
   const { t } = useTranslation();
-
 
   const keyMap: Record<string, string> = {
     ALLOW_NEW_USER: 'display_user_inscription_page',
@@ -20,17 +26,18 @@ export const RenderInput = ({ setting, handleChange }: IRenderInputProps) => {
   const label = t(tKey, { defaultValue: setting.key });
   const tooltip = t(`${tKey}_tooltip`, { defaultValue: '' });
 
-
-  if (setting.key === 'MAX_UPLOAD_SIZE') {
+  if (setting.key === SettingKeys.MAX_UPLOAD_SIZE) {
     return (
       <Tooltip title={tooltip} placement="top" arrow>
         <TextField
           type="number"
           label={label}
           value={setting.value}
-          onChange={e => handleChange(setting.id, e.target.value)}
+          onChange={(e) => handleChange(setting.id, e.target.value)}
           InputProps={{
-            endAdornment: <InputAdornment position="end">{t('mb')}</InputAdornment>,
+            endAdornment: (
+              <InputAdornment position="end">{t('mb')}</InputAdornment>
+            ),
           }}
           inputProps={{ min: 1 }}
         />
@@ -44,7 +51,9 @@ export const RenderInput = ({ setting, handleChange }: IRenderInputProps) => {
           control={
             <Switch
               checked={setting.value === 'true'}
-              onChange={e => handleChange(setting.id, e.target.checked.toString())}
+              onChange={(e) =>
+                handleChange(setting.id, e.target.checked.toString())
+              }
             />
           }
           label={label}
@@ -58,8 +67,8 @@ export const RenderInput = ({ setting, handleChange }: IRenderInputProps) => {
         fullWidth
         label={label}
         value={setting.value}
-        onChange={e => handleChange(setting.id, e.target.value)}
+        onChange={(e) => handleChange(setting.id, e.target.value)}
       />
     </Tooltip>
   );
-}
+};
