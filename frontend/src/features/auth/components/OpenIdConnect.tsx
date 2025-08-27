a2d2c298af7264479b027bdc9f7d7e9a630ebedd
Drilio <Roy-Antoine93@proton.me>
2025-08-27 08:09:12 +0200

    Feedback for user when an error with openIdConnect occurred

--- frontend/src/features/auth/components/OpenIdConnect.tsx
+++ frontend/src/features/auth/components/OpenIdConnect.tsx
@@ -2,10 +2,15 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect, useRef } from 'react';
 import { LoadingSpinner } from '../../../components/elements/loadingSpinner.tsx';
 import { useOpenIdConnectLogin } from '../hooks/useOpenIdConnectLogin.ts';
+import toast from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
 
 export const OpenIdConnect = () => {
   const navigate = useNavigate();
-  const { mutateAsync } = useOpenIdConnectLogin();
+  const { t } = useTranslation();
+
+  const { mutateAsync: tryToConnectWithOpenIdConnect } =
+    useOpenIdConnectLogin();
   const hasRun = useRef(false);
 
   useEffect(() => {
@@ -23,9 +28,10 @@ export const OpenIdConnect = () => {
       }
 
       try {
-        await mutateAsync({ code, redirectUri });
+        await tryToConnectWithOpenIdConnect({ code, redirectUri });
         navigate('/app/my-projects');
       } catch (err) {
+        toast.error(t('error_openIdConnect_failure'));
         console.error('❌ OIDC login failed:', err);
         navigate('/login');
       }
