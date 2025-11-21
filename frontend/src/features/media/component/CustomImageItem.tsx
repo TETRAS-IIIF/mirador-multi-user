012ec4662098f9117e0ab0b842e32b3e32fc2f0c
Drilio <117070163+Drilio@users.noreply.github.com>
2025-11-21 14:50:47 +0100

    Revisions after develop merge (#725)
    * FIX remove borders
    * FIX Traefik x-frame rule to allow over origin
    * FIX Render issue with edited text media
    * FIX Media Height
    * REMOVE Remove simple text editor

--- frontend/src/features/media/component/CustomImageItem.tsx
+++ frontend/src/features/media/component/CustomImageItem.tsx
@@ -2,6 +2,15 @@ import { styled } from '@mui/material';
 
 export const CustomImageItem = styled('div')({
   position: 'relative',
+  height: 200,
+  overflow: 'hidden',
+
+  '& img': {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+
   '&:hover img': {
     opacity: 0.4,
   },
