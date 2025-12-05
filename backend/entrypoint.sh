ad66ff3d0a9de31f4dfe4788aec8e36e84d1bbb7
Drilio <Roy-Antoine93@proton.me>
2025-12-05 11:56:54 +0100

    UPDATE nest.js and remove major security issues

--- backend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Reinstall bcrypt
-echo "Reinstalling bcrypt..."
-npm uninstall bcrypt
-npm install bcrypt
-#
-## Start the application
-echo "Starting the application..."
-npm run start:debug
