81cf29f6d2993d2ca89e4234bebfbfe7083d796f
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2025-12-10 17:04:24 +0100

    Revert "Add error log"
    This reverts commit f316bbf9986c1bf26c5024c3d25bedf1c061f458.

--- backend/src/BaseEntities/annotation-page/annotation-page.service.ts
+++ backend/src/BaseEntities/annotation-page/annotation-page.service.ts
@@ -61,7 +61,6 @@ export class AnnotationPageService {
   }
 
   async findAll(annotPageId: string, projectId: number) {
-    this.logger.error('Finding all annotation pages', 'TRACE');
     try {
       return await this.annotationPageRepository.find({
         where: { annotationPageId: annotPageId, projectId: projectId },
