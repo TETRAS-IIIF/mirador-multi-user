f39a16e85f7a155f425b6e22680920363de8e2aa
Drilio <Roy-Antoine93@proton.me>
2026-01-19 15:02:25 +0100

    FIX vitest running e2e test

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   expect: {
     timeout: 200_000,
   },
-
+  testMatch: '**/*.e2e.ts',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
