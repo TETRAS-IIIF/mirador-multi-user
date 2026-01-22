49097adf0a6b77458a4ff0860b2bf88dca623ce0
Drilio <117070163+Drilio@users.noreply.github.com>
2026-01-22 08:24:58 +0100

    Center loading spinner (#779)

--- frontend/src/providers/app-provider.tsx
+++ frontend/src/providers/app-provider.tsx
@@ -3,10 +3,25 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { LoadingSpinner } from '../components/elements/loadingSpinner.tsx';
 import { queryClient } from '../lib/react-query.ts';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { Box } from '@mui/material';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <Suspense fallback={<LoadingSpinner />}>
+    <Suspense
+      fallback={
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <LoadingSpinner />
+        </Box>
+      }
+    >
+      {' '}
       <QueryClientProvider client={queryClient}>
         <Router
           future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
