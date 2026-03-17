45ac4d6144a87b4a27c11d3239c11617270e9e1b
Drilio <117070163+Drilio@users.noreply.github.com>
2026-03-17 11:56:11 +0100

    FIX error handling on oidc login (#805)

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -309,7 +309,7 @@ export class AuthService {
         url: '',
         user: { email: '', id: 0, name: '' },
         message: `an error occured while creating or login user with openIdConnect protocol with OIDC_CLIENT_ID : ${process.env.OIDC_CLIENT_ID}, redirectUri : ${redirectUri}
-, code : ${code}`,
+, error : ${error}`,
       });
     }
   }
